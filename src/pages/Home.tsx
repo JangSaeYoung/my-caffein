b89cdb680af58aa3f4939a1d2eb3047f06029d66
saeyoung <syoungbb25@gmail.com>
2022-06-02 01:22:56 +0900

    Feat: type.tsx 라우터 기능 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ const Home = () => {
     <div>
         <Memo>홈 화면입니다.</Memo>
         <Link to="/CampDetail">캠프자세히보기</Link>
+        <Link to="/CommunityDetail">커뮤니티자세히보기</Link>
     </div>
     );
 };
--- src/types/type.tsx
+++ src/types/type.tsx
@@ -1,11 +1,22 @@
-const Type = () => {
-    return (
-    <div>
-        <h1>홈</h1>
-        <p>가장 먼저 보여지는 페이지입니다.</p>
-    </div>
-    );
-};
+export interface Camp {
+    id: number;
+    name: string;
+    type: "popular" | "sale";
+    status?: "모집전" | "모집중" | "모집완로";
+    startDate: string;
+    thumbnail: string;
+}
 
-export default Type;
+export interface Camp{
+    field?: string;
+    skill?: string;
+}
 
+// let case1:Camp ={
+//     id: 0,
+//     name: "React 실무 프로젝트 역량 업그레이드",
+//     type: "popular",
+//     status: "모집중",
+//     startDate: "1월 9일",
+//     thumbnail: "URL",
+// }
