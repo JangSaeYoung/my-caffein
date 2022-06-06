bfbd49e604e32f51093901c8ce290bd500e3d02b
saeyoung <syoungbb25@gmail.com>
2022-06-07 00:55:53 +0900

    [Feat] type.tsx : interface 타입 수정

--- src/types/type.tsx
+++ src/types/type.tsx
@@ -1,26 +1,34 @@
+export type CampType = "popular" | "sale" ;
+
 export interface InCamp {
     id: number;
     name: string;
     type: string;
-    status?: "모집전" | "모집중" | "모집완로"; 
+    status: string; 
     field: string;
     skill: string;
     startDate: string;
     thumbnail: string;
-    title: string;
 }
 
 export interface InCommunity{
+    // id: number;
+    // tags: string[];
+    // title: string; //제목 
+    // questions: string; //질문 
+    // comments: IComment[];
+
     id: number;
-    tags: string;  //상단태그들
-    title: string; //제목 
-    questions: string; //질문
+    tags: string;
+    title: string; 
+    questions: string; 
+    comments: string;
+
 }
 
-export interface InComent{
-    id: number;
-    img: string; //프로필 이미지 주소 string으로 받는다.
-    name: string; //코멘트 달아준 사람 이름/닉네임
-    answer: string; //코멘트 내용
+export interface IComment {
+  profile: string;
+  nickname: string;
+  content: string;
 }
                         
