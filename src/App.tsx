ca1c2bca63cfe0f1ee2dc32cda71ee48ff424f13
saeyoung <syoungbb25@gmail.com>
2022-06-14 14:27:23 +0900

    [Chore] package.json: reset-css 설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import Home  from './pages/Home';
 import CampDetail from './pages/CampDetail';
 import CommunityDetail from './pages/CommunityDetail';
+import GlobalStyled from "./styles/global";
+
 
 
 
 const App = () => {
   return (
     <>
+    <GlobalStyled />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/CampDetail" element={<CampDetail />} />
