d693472603452df9fb33f6802857dff104c7bb81
saeyoung <syoungbb25@gmail.com>
2022-06-16 15:38:45 +0900

    [Feat]Banner: ClassName변경, 디자인과 레이아웃 구현

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,29 +1,24 @@
 import styled from "styled-components";
+import colors from "styles/colors";
+import fonts from "styles/fonts";
 
 const Banner = () => {
   return (
-    <BannerContainer>
+    <Container>
       <div className="banner-text">
         현직자와 소통하며 배우는 실무 스킬, 퍼스널 트레이닝
       </div>
-    </BannerContainer>
+    </Container>
   );
 };
 
 export default Banner;
 
-const BannerContainer = styled.div`
-  text-align: center;
-  background: #7471ff;
-
-  .banner-text {
-    font-family: "Noto Sans";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 24px;
-    line-height: 34px;
-    letter-spacing: -0.01em;
-    color: #ffffff;
-    padding: 33px 394px 33px 42px;
-  }
+const Container = styled.div`
+  max-width: 960px;
+  padding: 30px 40px;
+  background-color: ${colors.primary1};
+  color: ${colors.white};
+  ${fonts.H1};
+  margin: 50px 0;
 `;
