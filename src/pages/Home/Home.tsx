96d4a62149075b63e3e68ddb671a8e112a80e90c
saeyoung <syoungbb25@gmail.com>
2022-06-16 15:15:58 +0900

    [Feat]Home: className 변경 및 import 절대경로 설정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,39 +1,32 @@
 // import { Link } from 'react-router-dom';
 import styled from "styled-components";
-import Navigation from "../../components/Navigation/Navigation";
-import Footer from "../../components/Footer/Footer";
 import React, { useState, useEffect } from "react";
-import CommuContainer from "./Components/CommunitySection";
-import Banner from "../../components/Banner/Banner";
-import CampContainer from "./Components/CampSection";
-import Slide from "../../components/Slide/Slide";
-import Header from "./Components/HeaderSection";
-
+import { Banner, Footer } from "components";
 import { CampSection, HeaderSection, CommunitySection } from "./Components";
 
 const Home = () => {
   return (
-    <HomeContainer>
+    <Container>
       <HeaderSection />
-      <div className="mainContainer">
+      <MainContainer>
         <CampSection title={""} camps={[]} />
         <Banner />
         <CommunitySection title={""} commus={[]} />
-      </div>
+      </MainContainer>
       <Footer />
       {/* <Link to="/CampDetail">캠프자세히보기</Link>
         <Link to="/CommunityDetail">커뮤니티자세히보기</Link> */}
-    </HomeContainer>
+    </Container>
   );
 };
 
 export default Home;
 
-const HomeContainer = styled.div`
+const Container = styled.div`
   margin: 0 auto;
+`;
 
-  .maincontainer {
-    max-width: 960px;
-    background-color: pink;
-  }
+const MainContainer = styled.div`
+  max-width: 960px;
+  margin: 50px 50px 0 50px;
 `;
