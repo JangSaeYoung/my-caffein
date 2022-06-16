54cd983119329388d49c4a4b8f2e0d3f304dea0d
saeyoung <syoungbb25@gmail.com>
2022-06-17 02:58:21 +0900

    [Feat]CommunityCard: 카드 레이아웃 구성

--- src/components/CommunityCard/CommunityCard.tsx
+++ src/components/CommunityCard/CommunityCard.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { InCommunity } from "../../types/type";
 import { Link } from "react-router-dom";
+import fonts from "styles/fonts";
 
 interface IncoProps {
   commu: InCommunity;
@@ -8,11 +9,11 @@ interface IncoProps {
 
 const CommunityCard = ({ commu }: IncoProps) => {
   return (
-    <CardContainer>
-      <div className="comu-content">
-        <div className="commu-tag">{commu.tags}</div>
-        <div className="commu-title">{commu.title}</div>
-        <div className="commu-questions">{commu.questions}</div>
+    <Container>
+      <div className="content">
+        <div className="tag">{commu.tags}</div>
+        <div className="title">{commu.title}</div>
+        <div className="questions">{commu.questions}</div>
         <div className="commentBox">
           <div className="commu-comments">{commu.comments}</div>
         </div>
@@ -20,103 +21,39 @@ const CommunityCard = ({ commu }: IncoProps) => {
       <Link to="/CommunityDetail">
         <div className="plusBtn">+더보기</div>
       </Link>
-    </CardContainer>
+    </Container>
   );
 };
 
 export default CommunityCard;
 
-const CardContainer = styled.div`
-  /* border: 1px solid #CCCCCC;
-border-radius: 10px;
-
-
-position: relative;
-  height: 280px;
-  background-color: #FFFFFF;
-  border-radius: 10px;
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  width: 224px;
+  height: 320px;
+  box-sizing: border-box;
+  background-color: #6bcbb8;
+  border-radius: 10px;
+  padding: 16px;
 
-  .comu-content {
-    padding: 16px;
-    padding-bottom: 0px;
-    z-index: 1;
-    
-  }
- 
-  .commu-tag {
-    width: 67px;
-    height: 22px;
-    border-radius: 4px;
-    background-color: #7471FF;
-    text-align: center;
-    font-family: 'Noto Sans';
-font-style: normal;
-font-weight: 700;
-font-size: 12px;
-line-height: 14px;
-
-letter-spacing: -0.01em;
-
-color: #FFFFFF;
-display: flex;
-justify-content: center;
-align-items: center;
-   
+  .content {
+    color: white;
   }
 
-  .commu-title{
-      width: 191px;
-      height: 36px;
-      font-family: 'Noto Sans';
-font-style: normal;
-font-weight: 700;
-font-size: 16px;
-line-height: 18px;
-letter-spacing: -0.01em;
-
+  .tag {
+    padding-bottom: 8px;
   }
 
-  .commu-questions {
-    width: 191px;
-    height: 36px;
-    font-family: 'Noto Sans CJK KR';
-font-style: normal;
-font-weight: 400;
-font-size: 14px;
-line-height: 16px;
-
-letter-spacing: -0.01em;
+  .title {
+    ${fonts.Body1}
+    font-weight: bold;
+    padding-bottom: 10px;
   }
-  
 
-
-.commentBox{
-    background-color: red;
-    margin-top: 67px;
-    margin-bottom: 47px;
-    display: flex;
-    flex-direction: column;
-align-items: flex-start;
-padding: 0px;
-}
-
-
-.plusBtn{
-    text-align: center;
-    height: 14px;
-width: 40.73px;
-
-margin-left: 86.5px;
-margin-right: 96.73px;
-padding-bottom: 16px;
-font-family: 'Roboto';
-font-style: normal;
-font-weight: 400;
-font-size: 12px;
-line-height: 14px;
-color: #24DBAF;
-} */
+  .questions {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 18px;
+  }
 `;
